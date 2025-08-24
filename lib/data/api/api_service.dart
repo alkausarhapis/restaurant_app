a79b2453b44f5b759252a9a9d14763308cddc256
alkausarhapis <alkausarhapis@gmail.com>
2025-08-24 09:22:23 +0700

    feat: api services & states

---
+++ lib/data/api/api_service.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:restaurant_app/data/model/restaurant_detail_response.dart';
+import 'package:restaurant_app/data/model/restaurant_list_response.dart';
+
+class ApiService {
+  static const String _baseUrl = 'https://restaurant-api.dicoding.dev/';
+
+  Future<RestaurantListResponse> getRestaurantList() async {
+    final response = await http.get(Uri.parse('$_baseUrl/list'));
+    if (response.statusCode == 200) {
+      return RestaurantListResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load restaurant list');
+    }
+  }
+
+  Future<RestaurantDetailResponse> getRestaurantDetail(String id) async {
+    final response = await http.get(Uri.parse('$_baseUrl/detail/$id'));
+    if (response.statusCode == 200) {
+      return RestaurantDetailResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load restaurant detail');
+    }
+  }
+
+  Future<RestaurantListResponse> searchRestaurants(String query) async {
+    final response = await http.get(Uri.parse('$_baseUrl/search?q=$query'));
+    if (response.statusCode == 200) {
+      return RestaurantListResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to search restaurants');
+    }
+  }
+}
---
+++ lib/static/restaurant_detail_result_state.dart
@@ -0,0 +1,17 @@
+import 'package:restaurant_app/data/model/restaurant.dart';
+
+sealed class RestaurantDetailResultState {}
+
+class RestaurantDetailNoneState extends RestaurantDetailResultState {}
+
+class RestaurantDetailResultLoading extends RestaurantDetailResultState {}
+
+class RestaurantDetailErrorState extends RestaurantDetailResultState {
+  final String error;
+  RestaurantDetailErrorState(this.error);
+}
+
+class RestaurantDetailLoadedState extends RestaurantDetailResultState {
+  final Restaurant data;
+  RestaurantDetailLoadedState(this.data);
+}
