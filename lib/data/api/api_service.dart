1a990de17eb8b62edba569e3659c701055092cff
alkausarhapis <alkausarhapis@gmail.com>
2025-08-25 17:36:07 +0700

    chore: reorder import & remove unnecessary type

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -3,12 +3,12 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:restaurant_app/data/model/detail/restaurant_detail_response.dart';
 import 'package:restaurant_app/data/model/restaurant_list_response.dart';
-import 'package:restaurant_app/data/model/review/restaurant_review_response.dart';
 import 'package:restaurant_app/data/model/restaurant_search_response.dart';
+import 'package:restaurant_app/data/model/review/restaurant_review_response.dart';
 
 class ApiService {
-  static const String _baseUrl = 'https://restaurant-api.dicoding.dev/';
-  static const Duration timeLimit = Duration(seconds: 10);
+  static const _baseUrl = 'https://restaurant-api.dicoding.dev/';
+  static const timeLimit = Duration(seconds: 10);
 
   Future<RestaurantListResponse> getRestaurantList() async {
     final response = await http
